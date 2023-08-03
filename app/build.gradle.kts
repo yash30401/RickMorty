a1a081dc46759bd992a9ef9eb22ae2c57d223e5c
Yashveer singh <yashveersingh30401@gmail.com>
2023-08-03 20:53:50 +0530

    Initial commit All Dependencies added

---
+++ app/build.gradle.kts
@@ -0,0 +1,91 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.apollographql.apollo3").version("3.7.3")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.devyash")
+    }
+}
+
+android {
+    namespace = "com.devyash.rickmorty"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.devyash.rickmorty"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.apollographql.apollo3:apollo-runtime:3.7.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
+
+    //Lifecycle Scope
+    val lifecycle_version = "2.4.0"
+    val activityVersion = "1.4.0"
+    val coroutines = "1.5.2"
+    val kotlin_version = "1.5.31"
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    kapt ("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-service:$lifecycle_version")
+
+    implementation ("androidx.activity:activity-ktx:$activityVersion")
+
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+    api ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines")
+    api ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
+
+}
+
+kapt{
+    correctErrorTypes = true
+}
